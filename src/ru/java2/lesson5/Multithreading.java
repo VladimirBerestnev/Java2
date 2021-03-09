89038b7a04c2b10c2ec3f2fc76f0682d09ac592c
Vladimir <berestnew@yandex.ru>
2021-03-09 23:06:21 +0300

    HomeWork5-2

--- src/ru/java2/lesson5/Multithreading.java
+++ src/ru/java2/lesson5/Multithreading.java
@@ -1,5 +1,7 @@
 package ru.java2.lesson5;
 
+import java.util.Arrays;
+
 public class Multithreading {
 
     static final int size = 10000000;
@@ -12,17 +14,63 @@ public class Multithreading {
 
         for (int i = 0; i < size; i++){
             arr[i] = 1;
- //           System.out.println("i = " + i);
 
         }
+        firstMethod(arr);
+        secondMethod(arr);
+
+    }
+
+    private static void firstMethod(float[] arr) {
         long a = System.currentTimeMillis();
         for (int i = 0; i < size; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            System.out.println(i + " = " + arr[i]);
+
         }
         long b = System.currentTimeMillis();
         System.out.println((float)(b-a)/1000 + " секунд");
+        System.out.println(arr[size-2]);
+
+ //       System.out.println("Massive1 " + Arrays.toString(arr));
+    }
+
+
+    private static void secondMethod(float[] arr) {
+
+        float[] arr1 = new float[size];
+        float[] arr2 = new float[size];
+        long a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, arr1, 0, h);
+        System.arraycopy(arr, h, arr2, 0, h);
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 0; i < h; i++) {
+                    arr1[i] = (float)(arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < h; i++) {
+                    arr2[i] = (float)(arr2[i] * Math.sin(0.2f + (i + h) / 5) * Math.cos(0.2f + (i + h) / 5) * Math.cos(0.4f + (i + h) / 2));
+
+                }
+            }
+        });
+
+        System.arraycopy(arr1, 0, arr, 0, h);
+        System.arraycopy(arr2, 0, arr, h, h);
+
+        long b = System.currentTimeMillis();
+        System.out.println((float)(b-a)/1000 + " секунд");
+        System.out.println(arr[size-2]);
+  //      System.out.println("Massive2 " + Arrays.toString(arr));
 
     }
+
 
 }
